8a69dc0c277d973d46e353b063755730016a4bb1
jeremiedubuis <jeremie.dubuis@gmail.com>
2019-05-23 01:03:16 +0200

    :wrench: Update devops

--- src/api/config.js
+++ src/api/config.js
@@ -1,6 +1,8 @@
 export const secret = 'mysecretstring';
+
+const host = process && process.env && process.env.SQL_SERVER ? `${process.env.SQL_SERVER}:${process.env.SQL_PORT}` : 'localhost';
 export const mysql = {
-    host: 'localhost',
+    host,
     db: 'permaculture',
     user: 'root',
     password: ''
